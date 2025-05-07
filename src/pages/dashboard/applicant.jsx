81d4a833bb396b9b693665190ee6988f3c76533c
unknown <markstevensibayan11@gmail.com>
2025-05-07 21:45:38 +0530

    chore: sample commit

---
+++ src/pages/dashboard/applicant.jsx
@@ -0,0 +1,12 @@
+import styles from "./Dashboard.module.css";
+
+export default function Applicant_Dashboard() {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Sample Applicant Dashboard</h1>
+      <div className={styles.cardGrid}>
+        
+      </div>
+    </div>
+  );
+}
