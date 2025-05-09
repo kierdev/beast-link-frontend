ae817a16e153bf775d6528249b4218bfb8a51774
unknown <markstevensibayan11@gmail.com>
2025-05-09 07:07:12 +0530

    feat: dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Dashboard from "./pages/dashboard/dashboard";
 import Applicant_Dashboard from "./pages/dashboard/applicant";
+import Faculty_Dashboard from "./pages/dashboard/faculty";
+import Chairperson_Dashboard from "./pages/dashboard/chairperson";
+import Admin_Dashboard from "./pages/dashboard/admin";
+
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Admin_Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/applicant" element={<Applicant_Dashboard />} />
+        
+        {/* <Route path="/admin" element={<Admin_Dashboard />} />
+        <Route path="/chairperson" element={<Chairperson_Dashboard />} />
+        <Route path="/faculty" element={<Faculty_Dashboard />} />
+        <Route path="/admin" element={<Admin_Dashboard />} /> */}
       </Routes>
     </Router>
   );
